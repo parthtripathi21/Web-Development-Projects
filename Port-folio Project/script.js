4635069a8d2d445ad0839f97e5b09aa2200bfd95
Parth Tripathi <parthdadhich15augast@gmail.com>
2025-03-18 15:11:52 +0530

    Add files via upload

---
+++ Port-folio Project/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent page reload
+    alert("Message Sent Successfully!");
+});
