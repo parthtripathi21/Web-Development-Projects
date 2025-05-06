7ab0a256a6cb31da8d30c24d7d5546282b88619f
Parth Tripathi <parthdadhich15augast@gmail.com>
2025-05-06 21:54:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const themeToggleButton=document.getElementById('theme-button');const themeStylesheet=document.getElementById('theme-stylesheet');themeToggleButton.addEventListener('click',function(){if(themeStylesheet.getAttribute('href')==='style.css'){themeStylesheet.setAttribute('href','styles.css');document.getElementById("theme-button").innerHTML="click if you want to see light theme"}else{themeStylesheet.setAttribute('href','style.css');document.getElementById("theme-button").innerHTML="click only if you want to see dark theme"}})
+const themeToggleButton = document.getElementById('theme-button');
+const themeStylesheet = document.getElementById('theme-stylesheet');
+
+themeToggleButton.addEventListener('click', function () {
+    const currentTheme = themeStylesheet.getAttribute('href');
+
+    if (currentTheme === 'style.css') {
+        themeStylesheet.setAttribute('href', 'styles.css');
+        themeToggleButton.innerHTML = 'Click if you want to see light theme';
+    } else {
+        themeStylesheet.setAttribute('href', 'style.css');
+        themeToggleButton.innerHTML = 'Click only if you want to see dark theme';
+    }
+});
